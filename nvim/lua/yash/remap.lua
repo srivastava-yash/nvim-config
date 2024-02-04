525fb8befbfd89cf5397ecc6f70a8fae1ac76ddb
srivastava-yash <yash.184040@gmail.com>
2024-02-04 13:27:04 -0500

    window management remaps added

--- nvim/lua/yash/remap.lua
+++ nvim/lua/yash/remap.lua
@@ -49,3 +49,9 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
+vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+
