98c53ea35f0eb15db0cfa8b9c3de8e0e067aceeb
Yash Srivastava <yash.184040@gmail.com>
2025-04-18 19:33:14 -0700

    Update telescope.lua to add live grep and document symbols

--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -21,9 +21,8 @@ return {
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, {})
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
         vim.keymap.set('n', '<leader>gr', builtin.lsp_references, { noremap = true, silent = true })
